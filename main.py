dc30d9feba982816460d1fea0a863466aae8c6fa
lewisj2501 <jones.lewis2501@gmail.com>
2023-01-05 17:26:28 +0000

    Project Initialisation
    This first commit to main has the sole purpose of initialising the Python interpreter with the venv folder that contains all the libraries and packages that we will need.

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+# create the URL to the page you want to scrape
+url = 'https://www.worldfootball.net/'
+
+# Make the http request to worldfootball.net
+response = requests.get(url)
+
+print(response)
+
+# parse the response
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Make the http request to worldfootball.net
+
+# Check the status code to make sure the request was successful
+if response.status_code == 200:
+    print(soup)
+else:
+    print("Request failed with status code:", response.status_code)
