d7911f8218c0c8af9b15e2c35849454598a02b1b
lewisj2501 <jones.lewis2501@gmail.com>
2023-01-07 00:07:02 +0000

    Main.js Change
    - Changed the URL to find list of teams sorted by name in alphabetical order.
    - Added soup.Find_all method to get table class with meta data associated with all the teams in the premier league
    - Added for loop to print each element to the console.

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import requests
 from bs4 import BeautifulSoup
 
 # create the URL to the page you want to scrape
-url = 'https://www.worldfootball.net/'
+url = 'https://www.worldfootball.net/players_list/eng-premier-league-2022-2023/nach-mannschaft/1/'
 
 # Make the http request to worldfootball.net
 response = requests.get(url)
 
-print(response)
-
 # parse the response
 soup = BeautifulSoup(response.text, 'html.parser')
+
+# finds table class with meta data associated with all the teams in the premier league
+teamsList_1 = soup.find_all(class_ = "standard_tabelle")
 
 # Make the http request to worldfootball.net
 
 # Check the status code to make sure the request was successful
 if response.status_code == 200:
-    print(soup)
+    for team1 in teamsList_1:
+        print(team1)
 else:
     print("Request failed with status code:", response.status_code)
