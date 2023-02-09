67a13ef1b2a15132b9db699115cc415ebd7abd63
lewisj2501 <81177454+lewisj2501@users.noreply.github.com>
2023-02-09 20:20:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+# working from branch lewis
+
 import requests
+import html
 from bs4 import BeautifulSoup
 
 # create the URL to the page you want to scrape
-url = 'https://www.worldfootball.net/'
+url = 'https://www.worldfootball.net/players_list/eng-premier-league-2022-2023/nach-mannschaft/1/'
 
 # Make the http request to worldfootball.net
 response = requests.get(url)
 
-print(response)
-
 # parse the response
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Make the http request to worldfootball.net
+# finds table class with meta data associated with all the teams in the premier league
+column_headers = soup.find_all("th")
+row_data = soup.find_all('table', class_='standard_tabelle')
 
 # Check the status code to make sure the request was successful
 if response.status_code == 200:
-    print(soup)
+    for row in row_data:
+        data = row.text.split()
+        col1 = data[0]
+        col2 = data[1]
+        col3 = data[2]
+        col4 = data[3]
+        col5 = data[4]
+        print(data)
+        print('break')
+
 else:
     print("Request failed with status code:", response.status_code)
